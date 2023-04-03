0dd5721a31a7b242e66f6f7bcb99e798f4a445ea
Ammar <ammarmar592@gmail.com>
2023-04-03 20:27:42 +0700

    todolist

--- src/Task.jsx
+++ src/Task.jsx
@@ -3,7 +3,6 @@ export const Task = (props) => {
     <div className="listItem">
       <h2>{props.taskName}</h2>
       <div className="button">
-        <button className="edit"> Edit </button>
         <button onClick={() => props.deleteTask(props.id)}>X</button>
       </div>
     </div>
